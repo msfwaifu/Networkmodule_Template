1fb36a9e79119fd9f33e90470361b5e33e741c07
Convery <convery10122@gmail.com>
2016-10-21 12:32:11 +0200

    Modified dllmain to use the correct exports.

--- Source/DllMain.cpp
+++ Source/DllMain.cpp
@@ -3,7 +3,7 @@
     License: MIT
     Started: 2016-10-21
     Notes:
-        Entrypoint for the plugin, exports per the 2016 standard.
+        Entrypoint for the module, exports per the 2016 standard.
 */
 
 #include "STDInclude.h"
@@ -11,47 +11,12 @@
 
 extern "C"
 {
-    EXPORT_ATTR void __cdecl onExtensionUnloading(void)
+    EXPORT_ATTR struct IServer *GetServerinstance(const char *Hostname)
     {
         /*
-            This export is called for each plugin when the game exists.
-            It should be used to save your configurations and similar info.
+            Return a new instance of a derived IServer class.
         */
-    }
-    EXPORT_ATTR void __cdecl onInitializationStart(void)
-    {
-        /*
-            This export is called when all other dlls and plugins have been loaded.
-            It should be used to do your initialization and .text segment modifications.
-        */
-    }
-    EXPORT_ATTR void __cdecl onInitializationComplete(void)
-    {
-        /*
-            This export is called either 3 seconds after the start or when
-            another plugin such as AyriaPlatform notifies the bootstrap loader.
-            It should be used to do your memory modifications.
-        */
-    }
-    EXPORT_ATTR void __cdecl onMessage(uint32_t Message, ...)
-    {
-        /*
-            This export is called when a message is broadcasted to all plugins.
-            As such you'll want to have a unique name for the messages your
-            plugin listens for. Example below.
-        */
-
-        std::va_list Variadic;
-        va_start(Variadic, Message);
-
-        // Messages are a 32bit FNV1a hash of a string.
-        switch (Message)
-        {
-            case AYRIA::FNV1::Compiletime::FNV1a_32("MyPlugin_DefaultCase"):
-            default: break;
-        }
-
-        va_end(Variadic);
+        return nullptr;
     }
 }
 
